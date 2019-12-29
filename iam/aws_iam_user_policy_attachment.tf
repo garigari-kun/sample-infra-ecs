230a0f1db778d28a1604a157e8ee27fa87a0a2c1
garigari-kun <keisuke.cs@gmail.com>
2019-12-29 15:28:30 +0900

    Added ec2 user

--- iam/aws_iam_user_policy_attachment.tf
+++ iam/aws_iam_user_policy_attachment.tf
@@ -2,3 +2,8 @@ resource "aws_iam_user_policy_attachment" "deploy-attach" {
   user = aws_iam_user.deploy-user.name
   policy_arn = aws_iam_policy.deploy.arn
 }
+
+ resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+   role = aws_iam_role.ecs_instance_role.name
+   policy_arn = aws_iam_policy.ecs_instance_policy.arn
+ }
